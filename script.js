03d7942c2a92d1120ef53d072bb7d81be254ed8a
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-31 12:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function calculateMinCost() {
   //your code here
    let value = (document.getElementById("rope-lengths").value).split(",");
+	
 	let cost = 0; 
 	// alert(value)
 	while(value.length>=2){
-		// value.sort();
-		alert(value);
+		value.sort(function(a, b){return a - b});
+		// alert(value+"--"+cost);
 		let count = +value[0] + +value[1]; 
 		cost = cost + count;
 		value = value.slice(2);
-		value.push(count);
-		// alert(value);
+		value.unshift(count);
+		
 	}
   
   
